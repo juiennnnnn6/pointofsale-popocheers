8acbbbc161f057879f07f5512af325eca768ed43
juiennnnnn6 <ucwusir@gmail.com>
2025-07-22 11:33:50 +0800

    清理重複檔案，添加簡化員工測試工具

--- check-employees.sql
+++
@@ -1,8 +0,0 @@
--- 檢查員工資料
-SELECT * FROM employees;
-
--- 檢查員工表結構
-SELECT column_name, data_type, is_nullable 
-FROM information_schema.columns 
-WHERE table_name = 'employees' 
-ORDER BY ordinal_position; 
